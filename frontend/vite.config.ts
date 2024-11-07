f763b55baeb043d8b1bdb858a2980b58e5b36053
Lars Magnus Opheim <84862660+mcflonilo@users.noreply.github.com>
2024-11-07 11:55:37 +0100

    gate

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,15 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    /*
     proxy: {
       '/api': {
-        target: 'http://localhost:8082', // Backend server
+        target: 'http://localhost:8000', // Gateway server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-
-     */
   },
 });
